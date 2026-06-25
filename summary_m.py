9cc767bec17ed87ba704d0e04c3daf5f8cdedbef
moshe <hyim695064@gmail.com>
2026-06-25 10:17:29 +0300

    second

--- summary_m.py
+++ summary_m.py
@@ -1,23 +1,32 @@
-agent ="bond"
-m_code = 3456
-distance_t_t = 5.5
-active_status = True
-print(agent, m_code, distance_t_t, active_status)
-print(type(agent,))
-print(type(m_code))
-print(type(distance_t_t))
-print(type(active_status))
-status = False
-print(not status)
-age = 20
-has_id = True
-if age >= 18 and has_id == True:
-    print("can enter")
+# agent ="bond"
+# m_code = 3456
+# distance_t_t = 5.5
+# active_status = True
+# print(agent, m_code, distance_t_t, active_status)
+# print(type(agent,))
+# print(type(m_code))
+# print(type(distance_t_t))
+# print(type(active_status))
+# status = False
+# print(not status)
+# age = 20
+# has_id = True
+# if age >= 18 and has_id == True:
+    # print("can enter")
+# else:
+    # print("cant enter")
+#givnage = int(input("what your age"))
+#if givnage >= 18:
+    # print("enter")
+#else:
+    #print("not welcome")
+temperature = 38.2
+if temperature >= 37.5:
+    print("high temperature")
 else:
-    print("cant enter")
-givnage = int(input("what your age"))
-if givnage >= 18:
-    print("enter")
+    print("normal temperature")
+number = int(input("what your number"))
+if number % 2 == 0:
+    print("even number")
 else:
-    print("not welcome")
-      
+    print("odd number")
